b1503177f56b4ff1b01cbafec625a03eb624af94
Julia Sun <Julia Sun>
2015-04-28 13:42:12 -0700

    added samantha's carousel, styled images

--- assets/js/app/controllers/SamanthaCtrl.js
+++ assets/js/app/controllers/SamanthaCtrl.js
@@ -1,12 +1,12 @@
 app.controller('SamanthaCtrl', ['$scope', function($scope) {
     $scope.pieces = [
 
-   		'../img/samantha/img2.jpg',
    		'../img/samantha/img3.jpg',
    		'../img/samantha/img1.jpg',
    		'../img/samantha/img4.jpg',
    		'../img/samantha/img5.jpg',
-   		'../img/samantha/img6.jpg'
+   		'../img/samantha/img6.jpg',
+   		'../img/samantha/img2.jpg'
 
     ];
 
