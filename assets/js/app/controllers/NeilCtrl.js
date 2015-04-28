07507126c6a83adef1bcc8ec23cf4b3d56f71786
Julia Sun <Julia Sun>
2015-04-28 15:20:47 -0700

    fixed overflow and added neil's portfolio

--- assets/js/app/controllers/NeilCtrl.js
+++ assets/js/app/controllers/NeilCtrl.js
@@ -1,4 +1,18 @@
 app.controller('NeilCtrl', ['$scope', function($scope) {
     // Add functionality for home page here
+    $scope.pieces = [
 
+   		'../img/neil/img2.jpg',
+   		'../img/neil/img3.jpg',
+   		'../img/neil/img1.jpg',
+   		'../img/neil/img4.jpg',
+   		'../img/neil/img5.jpg',
+   		'../img/neil/img6.jpg',
+   		'../img/neil/img7.jpg'
+
+    ];
+
+    $scope.goToSlide = function(index) {
+    	$scope.carouselIndex = index;
+    };
 }]);
